ec926f7be3e233adfe05bcdbff66a6a7d3ce2703
Desiree Ferreira <desireeferreira6@gmail.com>
2025-03-10 23:55:32 -0300

    Projeto Loja de Games - Backend

---
+++ src/main/java/com/generation/lojadegames/model/Produto.java
@@ -0,0 +1,58 @@
+package com.generation.lojadegames.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	  @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+	    private String nome;
+	    private double preco;
+	    
+	    @ManyToOne
+	    @JsonIgnoreProperties("produtos")	  	    
+	 private Categoria categoria;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public double getPreco() {
+			return preco;
+		}
+
+		public void setPreco(double preco) {
+			this.preco = preco;
+		}
+
+		public Categoria getCategoria() {
+			return categoria;
+		}
+
+		public void setCategoria(Categoria categoria) {
+			this.categoria = categoria;
+		}
+}
